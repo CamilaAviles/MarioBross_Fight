1c5b2803d30a886235a5ad34d0d64ea0f31315e7
Camila Aviles <camilanashv2003@gmail.com>
2023-06-15 19:58:11 -0600

    feat: the class fight is created, where its method is marios. The start_fight function is added, where two characters are chosen randomly from the list and fight until one of them runs out of life and fights against another character, the last character left in the list is the winner.

--- game1.py
+++ game1.py
@@ -20,3 +20,41 @@ class Mario:
 
     def __str__(self):
         return f"{self.name} (Level: {self.level})"
+
+
+class Fight:
+    def __init__(self, marios):
+        self.marios = marios
+
+    def start_Fight(self):
+        print("Fight starts!")
+        while len(self.marios) > 1:
+            mario1 = random.choice(self.marios)
+            mario2 = random.choice(self.marios)
+            if mario1 != mario2:
+                print(f"{mario1} vs {mario2}")
+                while mario1.current_life > 0 and mario2.current_life > 0:
+                    mario1.attack(mario2)
+                    if mario2.current_life > 0:
+                        mario2.attack(mario1)
+                if mario1.current_life > 0:
+                    print(f"{mario1} wins the fight against {mario2}!")
+                    self.marios.remove(mario2)
+                else:
+                    print(f"{mario2} wins the fight against {mario1}!")
+                    self.marios.remove(mario1)
+        print(f"{self.marios[0]} is the winner!!!!")
+
+
+# Create Mario instances
+marios = []
+marios.append(Mario("Mario", 10))
+marios.append(Mario("Luigi", 8))
+marios.append(Mario("Bowser", 12))
+marios.append(Mario("Princess Peach", 7))
+marios.append(Mario("Yoshi", 9))      
+
+
+# Create a Fight instance and start the fight
+Fight = Fight(marios)
+Fight.start_Fight()
